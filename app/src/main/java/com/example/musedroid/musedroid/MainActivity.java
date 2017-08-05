c92004fd51675d1f6faf96a584ae0d6d59f7a315
GBardis <crisolivasavatage@hotmail.com>
2017-08-05 20:15:28 +0300

    Remove toolbar and show museumname in actionbar

--- app/src/main/java/com/example/musedroid/musedroid/MainActivity.java
+++ app/src/main/java/com/example/musedroid/musedroid/MainActivity.java
@@ -15,8 +15,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listViewFromFirebase();
-        // firebase.createMuseum("1", new Museum("1", "museum akropolis", "The Acropolis Museum (Greek: Μουσείο Ακρόπολης, Mouseio Akropolis) is an archaeological museum focused on the findings of the archaeological site of the Acropolis of Athens.", "37.968450", "23.728523"));
-        //  firebase.createMuseum("2", new Museum("2", "museum goulandri", "The Goulandris Museum of Natural History is a museum in Kifisia, a northeastern suburb of Athens, Greece. It was founded by Angelos Goulandris and Niki Goulandris in 1965 in order to promote interest in the natural sciences, to raise the awareness of the public, in general, and in particular to call its attention to the need to protect Greece's natural wildlife habitats and species in the danger of extinction.", "38.074472", "23.814854"));
+        //firebaseHandler.createMuseum("1", new Museum("museum akropolis", "The Acropolis Museum (Greek: Μουσείο Ακρόπολης, Mouseio Akropolis) is an archaeological museum focused on the findings of the archaeological site of the Acropolis of Athens.", "37.968450", "23.728523"));
+        // firebaseHandler.createMuseum("2", new Museum("museum goulandri", "The Goulandris Museum of Natural History is a museum in Kifisia, a northeastern suburb of Athens, Greece. It was founded by Angelos Goulandris and Niki Goulandris in 1965 in order to promote interest in the natural sciences, to raise the awareness of the public, in general, and in particular to call its attention to the need to protect Greece's natural wildlife habitats and species in the danger of extinction.", "38.074472", "23.814854"));
     }
 
     // Use Firebase to populate a listview.
--- app/src/main/java/com/example/musedroid/musedroid/ShowActivity.java
+++ app/src/main/java/com/example/musedroid/musedroid/ShowActivity.java
@@ -2,20 +2,17 @@ package com.example.musedroid.musedroid;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 
 public class ShowActivity extends AppCompatActivity {
-    Toolbar mToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show);
 
-        mToolbar = (Toolbar) findViewById(R.id.toolbar);
         Bundle bundle = getIntent().getExtras();
-        if (bundle !=null){
-            mToolbar.setTitle(bundle.getString(""));
+        if (bundle != null) {
+            setTitle(bundle.getString(""));
         }
     }
 }
