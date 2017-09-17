643a083dc99c5b906e5c954f90aea053ff832554
teostergiou <theofilosst@hotmail.com>
2017-09-17 14:38:25 +0300

    Add Functionality in Splash Screen & Add png photos in mipmap in all densities

--- app/src/main/java/com/example/musedroid/musedroid/SplashActivity.java
+++ app/src/main/java/com/example/musedroid/musedroid/SplashActivity.java
@@ -1,13 +1,52 @@
 package com.example.musedroid.musedroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashActivity extends AppCompatActivity {
+import com.daimajia.androidanimations.library.Techniques;
+import com.viksaa.sssplash.lib.activity.AwesomeSplash;
+import com.viksaa.sssplash.lib.cnst.Flags;
+import com.viksaa.sssplash.lib.model.ConfigSplash;
+
+public class SplashActivity extends AwesomeSplash {
+
+    //DO NOT OVERRIDE onCreate()!
+    //if you need to start some services do it in initSplash()!
+
+    //https://github.com/ViksaaSkool/AwesomeSplash
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+    public void initSplash(ConfigSplash configSplash) {
+        /* you don't have to override every property */
+
+        //Customize Circular Reveal
+        configSplash.setBackgroundColor(R.color.colorPrimary); //any color you want form colors.xml
+        configSplash.setAnimCircularRevealDuration(2000); //int ms
+        configSplash.setRevealFlagX(Flags.REVEAL_LEFT);  //or REVEAL_RIGHT
+        configSplash.setRevealFlagY(Flags.REVEAL_TOP); //or REVEAL_BOTTOM
+
+        //Choose LOGO OR PATH; if you don't provide String value for path it's logo by default
+
+        //Customize Logo
+        configSplash.setLogoSplash(R.mipmap.ic_splash); //or any other drawable
+        configSplash.setAnimLogoSplashDuration(2000); //int ms
+        configSplash.setAnimLogoSplashTechnique(Techniques.ZoomIn); //choose one form Techniques (ref: https://github.com/daimajia/AndroidViewAnimations)
+
+
+        //Customize Title
+        configSplash.setTitleSplash(getString(R.string.app_name));
+        configSplash.setTitleTextColor(android.R.color.white);
+        configSplash.setTitleTextSize(30f); //float value
+        configSplash.setAnimTitleDuration(3000);
+        configSplash.setAnimTitleTechnique(Techniques.Landing);
+
+    }
+
+    @Override
+    public void animationsFinished() {
+        //transit to another activity here
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        finish();
     }
 }
