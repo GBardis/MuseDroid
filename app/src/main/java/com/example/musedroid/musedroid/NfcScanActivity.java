d23a2847e1c39bd9cfaa536e8135b2b1c3afcf2a
frcake <mpdevcake@gmail.com>
2017-10-24 16:25:26 +0300

    Implements nfc reading - testing pending

--- app/src/main/java/com/example/musedroid/musedroid/NfcScanActivity.java
+++ app/src/main/java/com/example/musedroid/musedroid/NfcScanActivity.java
@@ -1,25 +1,48 @@
 package com.example.musedroid.musedroid;
 
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
 import android.os.Bundle;
-import android.app.Activity;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-public class NfcScanActivity extends Activity {
-    public static final String TAG = "NfcDemo";
+public class NfcScanActivity extends AppCompatActivity implements Listener {
+    public static final String TAG = MainActivity.class.getSimpleName();
 
-    private TextView mTextView;
+    private EditText mEtMessage;
+    private Button mBtWrite;
+    private Button mBtRead;
+
+    //private NFCWriteFragment mNfcWriteFragment;
+    private NFCReadFragment mNfcReadFragment;
+
+    private boolean isDialogDisplayed = false;
+    private boolean isWrite = false;
+
     private NfcAdapter mNfcAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nfc_scan);
 
-        mTextView = (TextView) findViewById(R.id.textView_explanation);
+        //initViews
+        mEtMessage = (EditText) findViewById(R.id.et_message);
+        mBtWrite = (Button) findViewById(R.id.btn_write);
+        mBtRead = (Button) findViewById(R.id.btn_read);
 
+        //mBtWrite.setOnClickListener(view -> showWriteFragment());
+        mBtRead.setOnClickListener(view -> showReadFragment());
+
+        //initNFC
         mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
 
         if (mNfcAdapter == null) {
@@ -31,12 +54,79 @@ public class NfcScanActivity extends Activity {
         }
 
         if (!mNfcAdapter.isEnabled()) {
-            mTextView.setText("NFC is disabled.");
+            Toast.makeText(this, "NFC is disabled.", Toast.LENGTH_LONG).show();
         } else {
-            mTextView.setText(R.string.explanation);
+            Toast.makeText(this, "NFC is enabled.", Toast.LENGTH_LONG).show();
         }
 
         handleIntent(getIntent());
+    }
+
+
+    private void showReadFragment() {
+
+        mNfcReadFragment = (NFCReadFragment) getFragmentManager().findFragmentByTag(NFCReadFragment.TAG);
+
+        if (mNfcReadFragment == null) {
+
+            mNfcReadFragment = NFCReadFragment.newInstance();
+        }
+        mNfcReadFragment.show(getFragmentManager(),NFCReadFragment.TAG);
+
+    }
+
+    @Override
+    public void onDialogDisplayed() {
+
+        isDialogDisplayed = true;
+    }
+
+    @Override
+    public void onDialogDismissed() {
+
+        isDialogDisplayed = false;
+        isWrite = false;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+        IntentFilter ndefDetected = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        IntentFilter techDetected = new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED);
+        IntentFilter[] nfcIntentFilter = new IntentFilter[]{techDetected,tagDetected,ndefDetected};
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        if(mNfcAdapter!= null)
+            mNfcAdapter.enableForegroundDispatch(this, pendingIntent, nfcIntentFilter, null);
+
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(mNfcAdapter!= null)
+            mNfcAdapter.disableForegroundDispatch(this);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+
+        Log.d(TAG, "onNewIntent: "+intent.getAction());
+
+        if(tag != null) {
+            Toast.makeText(this, getString(R.string.message_tag_detected), Toast.LENGTH_SHORT).show();
+            Ndef ndef = Ndef.get(tag);
+
+            if (isDialogDisplayed) {
+
+                    mNfcReadFragment = (NFCReadFragment)getFragmentManager().findFragmentByTag(NFCReadFragment.TAG);
+                    mNfcReadFragment.onNfcDetected(ndef);
+            }
+        }
     }
 
     private void handleIntent(Intent intent) {
