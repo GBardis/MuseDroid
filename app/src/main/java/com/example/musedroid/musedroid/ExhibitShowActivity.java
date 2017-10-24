0311d6814a45822d506659009c9bb559aa70b85a
GBardis <crisolivasavatage@hotmail.com>
2017-10-24 18:24:02 +0300

    add firebase storage

--- app/src/main/java/com/example/musedroid/musedroid/ExhibitShowActivity.java
+++ app/src/main/java/com/example/musedroid/musedroid/ExhibitShowActivity.java
@@ -3,10 +3,12 @@ package com.example.musedroid.musedroid;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.preference.PreferenceManager;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.database.ChildEventListener;
@@ -14,6 +16,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Arrays;
 
@@ -30,6 +33,7 @@ public class ExhibitShowActivity extends AppCompatActivity {
     Context context;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference mDatabase = database.getReference();
+    ImageView exhibitImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +42,10 @@ public class ExhibitShowActivity extends AppCompatActivity {
         context = this;
         intent = getIntent();
         ExhibitShowActivity.exhibitId = intent.getStringExtra("exhibitId");
+        exhibitImage = findViewById(R.id.exhibitImage);
 
         if (savedInstanceState == null) {
+            getExhibitImage();
             getExhibitFields();
             userLanguage = userSettingsLang();
         }
@@ -109,6 +115,37 @@ public class ExhibitShowActivity extends AppCompatActivity {
             @Override
             public void onChildMoved(DataSnapshot dataSnapshot, String s) {
 
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void getExhibitImage() {
+        mDatabase.child("exhibits").child(ExhibitShowActivity.exhibitId).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String imageUrl = (String) dataSnapshot.child("imageUrl").getValue();
+                Uri uri = Uri.parse(imageUrl);
+                exhibitImage.setImageURI(uri);
+//                FirebaseStorage storage = FirebaseStorage.getInstance();
+//                StorageReference storageRef = storage.getReference();
+//                assert imageUrl != null;
+//
+//                storageRef.child("exhibits/" + ExhibitShowActivity.exhibitId + "/" + imageUrl).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+//                    @Override
+//                    public void onSuccess(Uri uri) {
+//                        exhibitImage.setImageURI(uri);
+//                    }
+//                }).addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception exception) {
+//                        // Handle any errors
+//                    }
+//                });
             }
 
             @Override
